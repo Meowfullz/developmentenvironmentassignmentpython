268a2295af230a021b645d47f6fa4a464f14a9ec
Meowfullz <spongegarsquarecob@gmail.com>
2023-08-30 00:02:13 -0400

    Development Environments Assignment

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from tv import *
+
+def main():
+    tv1 = tv(1,1,False)
+    print(tv1)
+    tv1.turnOn()
+    tv1.setChannel(30)
+    tv1.setVolume(3)
+    print(tv1)
+    
+    tv2 = tv(1, 1, False)
+    print(tv2)
+    tv2.turnOn()
+    tv2.channelUp()
+    tv2.channelUp()
+    tv2.channelUp()
+    tv2.channelDown()
+    tv2.volumeUp()
+    tv2.volumeUp()
+    tv2.volumeUp()
+    tv2.volumeDown()
+    print(tv2)
+    tv2.turnOff()
+
+    channel = input("PLease enter a channel between 1-120: ")
+    volume = input("PLease enter a volume level between 1-7: ")
+    tv3 = tv(int(channel), int(volume), True)
+    print(tv3)
+
+if __name__ == "__main__":
+    main()
